ffbe9c26a3add9d3a09cbb967a05fc63311724be
Jaime Segura <jseguraweb@gmail.com>
2020-01-12 15:31:43 +0100

    add header in form page

---
+++ script-form.js
@@ -0,0 +1,10 @@
+const toggleOptions = document.querySelector("#toggle-options");
+const optionsMenu = document.querySelector("#options-menu");
+
+toggleOptions.addEventListener("click", function(){
+    if(optionsMenu.style.display === "flex"){
+        optionsMenu.style.display = "none";
+    } else {
+        optionsMenu.style.display = "flex";
+    }
+});
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const toggleMenuButton = document.querySelector("#toggle-menu");
 const displayedMenu = document.querySelector(".menu");
 
-const toggleOptions = document.querySelector("#toggle-options");
-const optionMenu = document.querySelector(".options-menu");
-
 toggleMenuButton.addEventListener("click", function(e){
     if(displayedMenu.style.top === "7vh") {
         displayedMenu.style.top = "-100vh";
@@ -13,7 +10,3 @@ toggleMenuButton.addEventListener("click", function(e){
     e.preventDefault();
 });
 
-toggleOptions.addEventListener("click", function(e){
-    optionMenu.style.display === "flex" ? optionMenu.style.display = "none" : optionMenu.style.display = "flex";
-    e.preventDefault();
-});
