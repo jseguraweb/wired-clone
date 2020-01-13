049a3efd5daf5d724390c7f77ec824a24229e9a5
Jaime Segura <jseguraweb@gmail.com>
2020-01-13 14:21:54 +0100

    add first gift option

--- script-form.js
+++ script-form.js
@@ -8,3 +8,18 @@ toggleOptions.addEventListener("click", function(){
         optionsMenu.style.display = "flex";
     }
 });
+
+const optionalGift = document.querySelector(".optional-gift");
+
+const changeSelectedButton = (button) => {
+    button.style.border = "1px solid #057DBC";
+    button.style.borderRadius = "10px";
+    button.style.backgroundColor = "rgba(160, 209, 255, 0.541)";
+    button.style.boxShadow = "inset 2px 2px 5px #057DBC"
+    button.style.color = "#057DBC";
+    // optionalGift.firstElementChild.style.border = "solid #057DBC";
+};
+
+optionalGift.addEventListener("click", function(){
+    changeSelectedButton(optionalGift);
+})
